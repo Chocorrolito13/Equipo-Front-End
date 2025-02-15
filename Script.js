9bf2fb3e826a9d67dd7d947b930840f52fcac075
Kricke1 <enriqux.rdgz@gmail.com>
2025-02-14 21:36:36 -0600

    Se agregó el JS

--- Script.js
+++ Script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     const mensajeBienvenida = document.createElement("p");
-    mensajeBienvenida.textContent = "¡Descubre  ofertas especiales en instrumentos!";
+    mensajeBienvenida.textContent = "¡Descubre   especiales en instrumentos!";
     mensajeBienvenida.style.fontSize = "18px";
     mensajeBienvenida.style.fontWeight = "bold";
     mensajeBienvenida.style.color = "#333";
